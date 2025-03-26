7ee94b030ce6ea91ea93e87611c31a2dbb56bcf9
olha-dev-fullstack <olha.shevel.dev@gmail.com>
2025-03-26 13:05:23 +0200

    add auth

--- order_service/src/routes/cart.routes.ts
+++ order_service/src/routes/cart.routes.ts
@@ -3,30 +3,22 @@ import * as service from "../service/cart.service";
 import * as repository from "../repository/cart.repository";
 import { ValidateRequest } from "../utils/validator";
 import { CartRequestInput, CartRequestSchema } from "../dto/cartRequest.dto";
+import { RequestAuthorizer } from "./middleware";
 
 const router = express.Router();
 const repo = repository.CartRepository;
 
-const authMiddleware = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  // jwt
-  const isValidUser = true;
-  if (!isValidUser) {
-    res.status(403).json({ error: "authorization error" });
-    return;
-  }
-
-  next();
-};
-
 router.post(
   "/cart",
-  authMiddleware,
+  RequestAuthorizer,
   async (req: Request, res: Response, next: NextFunction) => {
     try {
+      const user = req.user;
+      if (!user) {
+        next(new Error("User not found"));
+        return;
+      }
+
       const error = ValidateRequest<CartRequestInput>(
         req.body,
         CartRequestSchema
@@ -37,50 +29,91 @@ router.post(
         return;
       }
 
+      const input: CartRequestInput = req.body;
+
       const response = await service.CreateCart(
-        req.body as CartRequestInput,
+        {
+          ...input,
+          customerId: user.id,
+        },
         repo
       );
       res.status(200).json(response);
       return;
     } catch (error) {
-      res.status(404).json({ error });
-      return;
+      next(error);
     }
   }
 );
 
-router.get("/cart", async (req: Request, res: Response, next: NextFunction) => {
-  // comes from our auth user parsed from JWT
-  const response = await service.GetCart(req.body.customerId, repo);
-  res.status(200).json(response);
-  return;
-});
+router.get(
+  "/cart",
+  RequestAuthorizer,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const user = req.user;
+      if (!user) {
+        next(new Error("User not found"));
+        return;
+      }
+      const response = await service.GetCart(user.id, repo);
+       res.status(200).json(response);
+       return
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 router.patch(
   "/cart/:lineItemId",
+  RequestAuthorizer,
   async (req: Request, res: Response, next: NextFunction) => {
-    const liteItemId = req.params.lineItemId;
-    const response = await service.EditCart(
-      {
-        id: +liteItemId,
-        qty: req.body.qty,
-      },
-      repo
-    );
-    res.status(200).json(response);
-    return;
+    try {
+      const user = req.user;
+      if (!user) {
+        next(new Error("User not found"));
+        return;
+      }
+
+      const liteItemId = req.params.lineItemId;
+      const response = await service.EditCart(
+        {
+          id: +liteItemId,
+          qty: req.body.qty,
+          customerId: user.id,
+        },
+        repo
+      );
+       res.status(200).json(response);
+       return
+    } catch (error) {
+      next(error);
+    }
   }
 );
 
 router.delete(
   "/cart/:lineItemId",
+  RequestAuthorizer,
   async (req: Request, res: Response, next: NextFunction) => {
-    const liteItemId = req.params.lineItemId;
-    console.log(liteItemId);
-    const response = await service.DeleteCart(+liteItemId, repo);
-    res.status(200).json(response);
-    return;
+    try {
+      const user = req.user;
+      if (!user) {
+        next(new Error("User not found"));
+        return;
+      }
+      const liteItemId = req.params.lineItemId;
+      console.log(liteItemId);
+      const response = await service.DeleteCart(
+        { customerId: user.id, id: +liteItemId },
+        repo
+      );
+       res.status(200).json(response);
+       return
+    } catch (error) {
+      next(error);
+    }
   }
 );
 
---
+++ order_service/src/routes/middleware.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+import { ValidateUser } from "../utils";
+
+export const RequestAuthorizer = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    if (!req.headers.authorization) {
+      res
+        .status(403)
+        .json({ error: "Unauthorized due to authorization token missing!" });
+      return;
+    }
+    const userData = await ValidateUser(req.headers.authorization as string);
+    req.user = userData;
+    next();
+  } catch (error) {
+    res.status(403).json({ error });
+    return;
+  }
+};
